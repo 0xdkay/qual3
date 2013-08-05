12ecadb7a175a4354a197b49d9a2ebb6d01b8862
dongkwan <insinoa@gmail.com>
2013-08-05 03:21:57 -0700

    change article font to NanumGothic

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,24 +1,49 @@
 function loginCheck(id, pw) 
 {
-	$.post('login', {
-		'id': id.value,
-	'pw': pw.value,
-	}, function(data) {
-		if(data=='true') {
-			$('#login-data').html("");
-			location.href = "/";
-		} else {
-			$('#login-data').html("Wrong id - password combination.");
-			$('#login-data').css("color","red");
-		}
-	});
-	return false;
+    $.post('login', {
+        'id': id.value,
+        'pw': pw.value,
+    }, function(data) {
+        if(data=='true') {
+            $('#login-data').html("");
+            location.href = "/";
+        } else {
+            $('#login-data').html("Wrong id - password combination.");
+            $('#login-data').css("color","red");
+        }
+    });
+    return false;
 }
 
-function forgetPass()
+function recoveryCheck(email)
 {
+    $.post('recovery', {
+        'email': email.value
+    }, function(data) {
+        if(data=='true') {
+            $('#login-data').html("");
+            location.href = "/";
+        } else {
+            $('#login-data').html("Something got WrongWrong id - password combination.");
+            $('#login-data').css("color","red");
+        }
+    });
+    return false;
 }
 
-function registerID()
+function registerCheck(id, pw, pw_confirm, key)
 {
+    $.post('register', {
+        'email': email.value
+    }, function(data) {
+        if(data=='true') {
+            $('#login-data').html("");
+            location.href = "/";
+        } else {
+            $('#login-data').html("Something got WrongWrong id - password combination.");
+            $('#login-data').css("color","red");
+        }
+    });
+    return false;
 }
+
