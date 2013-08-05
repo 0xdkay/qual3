de6c2510652a85d67140f1ecc6bb283bc507fa40
dongkwan <insinoa@gmail.com>
2013-08-04 20:17:02 -0700

    change deprecated variable options to settings

--- pages/auth.rb
+++ pages/auth.rb
@@ -2,41 +2,41 @@ require 'rubygems' if RUBY_VERSION < "1.9"
 require 'sinatra/base'
 
 module Sinatra
-	module SessionAuth
-		module Helpers
-			def authorized?
-				session[:id]
-			end
+    module SessionAuth
+        module Helpers
+            def authorized?
+                session[:id]
+            end
 
-			def authorized!
-				redirect '/login' unless authorized?
-			end
+            def authorized!
+                redirect '/login' unless authorized?
+            end
 
-			def logout!
-				session[:id] = nil
-			end
-		end
+            def logout!
+                session[:id] = nil
+            end
+        end
 
-		def self.registered(app)
-			app.helpers SessionAuth::Helpers
+        def self.registered(app)
+            app.helpers SessionAuth::Helpers
 
-			app.get '/logout' do
-				session[:id] = nil
-				redirect '/'
-			end
+            app.get '/logout' do
+                session[:id] = nil
+                redirect '/'
+            end
 
-			app.get '/login' do
-				redirect '/'  if authorized?
-			end
+            app.get '/login' do
+                redirect '/'  if authorized?
+            end
 
-			app.post '/login' do
-				if params[:id] == options.username && params[:pw] == options.password
-					session[:id] = params[:id]
-					"true"
-				else
-					"wrong"
-				end
-			end
-		end
-	end
+            app.post '/login' do
+                if params[:id] == settings.username && params[:pw] == settings.password
+                    session[:id] = params[:id]
+                    "true"
+                else
+                    "wrong"
+                end
+            end
+        end
+    end
 end
